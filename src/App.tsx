b67c6a88870e845f6e871e03dd13106541c5232b
Dongyeop <ddongyeop0129@gmail.com>
2024-10-21 23:38:57 +0900

    Simple Interface

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className='Container'>
+        <div className='ContainerBackPanel'>
+          <div className='TopPanel'>
+            <h5 className='SwapText'>스왑</h5>
+            <button className='SettingButton'/>
+          </div>
+          <CoinCount/>
+          <p className='Arrow'>↓</p>
+          <CoinCount/>
+          <button className='SwapButton'>스왑</button>
+        </div>
+      </div>
     </div>
   );
+}
+
+function CoinCount() {
+  return (
+    <div className='InputPanel'> 
+      <div className='Input'>
+        <input className='CountInput'></input>
+        <button className='CoinChange'>ETH</button>
+      </div>
+      <p className='USDText'>$0.0</p>
+    </div>
+  )
 }
 
 export default App;
