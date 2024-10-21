b10a5d62e9e61f5d500936383f3f105f3cac6126
Dongyeop <ddongyeop0129@gmail.com>
2024-10-21 23:58:13 +0900

    Swap css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './css/Swap.css'
+import './css/Select.css'
 
 function App() {
   return (
     <div className="App">
-      <div className='Container'>
-        <div className='ContainerBackPanel'>
-          <div className='TopPanel'>
+      <div className='Swap'>
+        <div className='SwapContainerBackPanel'>
+          <div className='SwapTopPanel'>
             <h5 className='SwapText'>스왑</h5>
             <button className='SettingButton'/>
           </div>
@@ -15,6 +17,11 @@ function App() {
           <p className='Arrow'>↓</p>
           <CoinCount/>
           <button className='SwapButton'>스왑</button>
+        </div>
+      </div>
+      <div className='Select'>
+        <div className='SelectContainerBackPanel'>
+          
         </div>
       </div>
     </div>
