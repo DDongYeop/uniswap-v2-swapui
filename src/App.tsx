e8b9e120912f242b356646afe420084c112a87ec
Dongyeop <ddongyeop0129@gmail.com>
2024-10-22 10:01:05 +0900

    Select UI

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 import './css/Swap.css'
 import './css/Select.css'
+import { log } from 'console';
+
+let isSwap = true;
 
 function App() {
   return (
     <div className="App">
-      <div className='Swap'>
+      <SwapPanel/>
+      <SelectPanel/>
+    </div>
+  );
+}
+
+function SwapPanel() {
+  if (!isSwap)
+    return null;
+
+  return (
+    <div className='Swap'>
         <div className='SwapContainerBackPanel'>
-          <div className='SwapTopPanel'>
-            <h5 className='SwapText'>스왑</h5>
-            <button className='SettingButton'/>
+          <div className='TopPanel'>
+            <h5 className='TopText'>스왑</h5>
+            <button className='TopButton'/>
           </div>
           <CoinCount/>
           <p className='Arrow'>↓</p>
@@ -19,25 +33,63 @@ function App() {
           <button className='SwapButton'>스왑</button>
         </div>
       </div>
-      <div className='Select'>
+  )
+}
+
+function SelectPanel() {
+
+  if (isSwap)
+    return null;
+  
+  return (
+    <div className='Select'>
         <div className='SelectContainerBackPanel'>
-          
+          <div className='TopPanel'>
+            <p className='TopText'>토큰 검색</p>
+            <button className='TopButton'/>
+          </div>
+          <input className='TextInput'></input>
+          <div className='RecentList'></div>
+          <div className='TokenScrollView'>
+            <Coin/>
+            <Coin/>
+            <Coin/>
+            <Coin/>
+            <Coin/>
+          </div>
+          <button className='TokenListManagement'>토큰 목록 관리</button>
         </div>
       </div>
-    </div>
-  );
+  )
 }
 
 function CoinCount() {
   return (
     <div className='InputPanel'> 
-      <div className='Input'>
-        <input className='CountInput'></input>
-        <button className='CoinChange'>ETH</button>
+      <div className='Input'>``
+        <input className='TextInput'></input>
+        <button className='CoinChange' onClick={PanelChange}>ETH</button>
       </div>
       <p className='USDText'>$0.0</p>
     </div>
   )
+}
+
+function Coin() {
+  return (
+    <button className='CoinButton'>
+      <div className='CoinImage'/>
+      <div className='CoinNameArea'>
+        <p className='CoinName'>ETH</p>
+        <p className='CoinFullName'>Ether</p>
+      </div>
+    </button>
+  )
+}
+
+function PanelChange() {
+  isSwap = !isSwap;
+  console.log(isSwap);
 }
 
 export default App;
